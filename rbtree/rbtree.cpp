474a85e630230ebe24a2d750347477583f260aba
YusyosoGIT <yusyoso@outlook.jp>
2020-08-17 15:06:45 +0900

    最初のコミット

---
+++ rbtree/rbtree.cpp
@@ -0,0 +1,193 @@
+/*
+ *	赤黒木（２分探索法）
+ *	生成＆出力プログラム
+ *	[コマンド] [ランダム生成要素数|数値リストのファイル]
+ *	[コマンド] [ランダム生成要素数]　[数値リストのファイル]
+ *
+ *	引数は１つか２つかを選ぶことが出来る。
+ *
+ *	引数が１つの時は要素数かファイル名を指定。
+ *		N個の要素の自動生成かファイルに記載の数値をもとに赤黒木の生成を行う
+ *	引数が２つの時は引数１に要素数　引数２にファイル名を指定する。
+ *		N個の要素を自動生成した後、ファイル記載の要素を足したり引いたりする
+ */
+
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <iomanip> // マニピュレータ
+#include "rbtree.h"
+
+#define E int
+using namespace std;
+//E elem[] = {0, 2, 9, 10, 12, 5, 3, 29, 20, 16};
+
+// 引数で受け取った赤黒木を標準出力する
+template<class T>
+class printer
+{
+	typedef pair<T, T> pr_val;
+	typedef map<int, pr_val> bmap;
+	class bmap::iterator it;// class はprinter<T>::bemap::iteratorの前に必要
+	bmap bough;
+	const rbtree<T> &tree;
+public:
+	printer(const rbtree<T> &rbt);
+	void out(const struct rbtree<T>::node *v, int dep, int lr);
+	void out() {out(tree.root, 0, 0);}
+};
+
+template<class T>
+printer<T>::printer(const rbtree<T> &rbt):
+	tree(rbt)
+{
+	cerr << hex << uppercase;
+}
+
+template<class T>
+void printer<T>::out(const struct rbtree<T>::node *v, int dep, int lr)
+{
+	if (v == tree.nil) {
+		if (lr != 2) return;
+		else {
+			bough.erase(dep);//長子不在の場合、親の枝をここで消す
+			return;
+		}
+	}
+	// いずれかの子がいる場合範囲登録
+	if (v->ch[0] != tree.nil && v->ch[1] != tree.nil) {
+		pr_val pr;
+		if (v->ch[1] == tree.nil) {
+			pr = make_pair(v->ch[0]->key, v->key);
+		}
+		else if (v->ch[0] == tree.nil) {
+			pr = make_pair(v->key, v->ch[1]->key);
+		}
+		else {
+			pr = make_pair(v->ch[0]->key, v->ch[1]->key);
+		}
+		bough.insert(make_pair(dep+1, pr));
+	}
+	out(v->ch[1], dep+1, 1);
+	// 枝部分の表示
+	for (int i=0; i< dep; i++) {
+		//iterator::bmap it= bough.find(i);
+		//bmap::iterator it;
+		it = bough.find(i);
+		if (it == bough.end() || v->key > it->second.second) {
+			cerr << "  ";
+		}
+		else {
+			cerr << "│ ";
+		}
+	}
+
+	//cerr << (!lr? "--": (lr == 1?  "「": "Ｌ"));
+	cerr << (!lr? "─ ": (lr == 1?  "┌ ": "└ "));
+	cerr << (v->red? "\x1b[31;1m": "\x1b[30;1m");// "1"はBoldのオプション
+	cerr << right << setfill('0') << setw(2) << v->key << endl;
+	cerr << "\x1b[0m";
+	if (lr == 2) bough.erase(dep);//長子の場合、親の枝は自分を表示したあとで消す
+	out(v->ch[0], dep+1, 2);
+}
+
+
+int main(int argc, char * argv[])
+{
+	// 要素ゼロのリスト生成
+    rbtree<E> tree;
+    // 出力プログラムに通知
+	printer<E> dsp(tree);
+
+    if (argc == 1) {
+        cout << "USAGE:" << argv[0] << " <arg1> [arg2]" << endl;
+        return 0;
+    }
+	// 要素のあるファイル名	
+	const char * entry;
+	int nElems;
+	nElems = atoi(argv[1]);
+	bool b1stArgNum = (nElems != 0);
+
+	switch ( argc ) 
+	{
+	case 2:
+		// 引数一つだけ
+		if (b1stArgNum)
+			// 引数が要素数／ファイル名なし
+			entry = NULL;
+		else
+			// 要素数なし／引数がファイル名
+			entry = argv[1];
+		break;
+
+	case 3:
+		// 引数二つ
+		if (!b1stArgNum) 
+			// 引数１の要素数が０のため異常終了
+			return 1;
+		else
+			// 引数１が要素数／引数２がファイル名
+			entry = argv[2];
+		break;
+
+	default:
+		// エラー
+		cout << "too may arguments" << endl;
+		return 1;
+	}
+
+	if (nElems)
+	{
+		// 要素数未設定の場合要素を設定
+		srand(0);
+		vector<int> deck;
+		deck.reserve(255);
+		for (int i= 0; i< 255; i++) {
+			deck.push_back(i);
+		}
+		for (int i= 0; i< nElems; i++) {
+			int index = rand() % deck.size();
+			tree.insert(deck[index]);
+			deck.erase(deck.begin() + index);
+		}
+	}
+
+	if (entry) {
+		// ファイルから要素を読み込む	
+		ifstream in(entry);
+/*	for (int i= 0, n= sizeof(elem)/ sizeof(E); i < n; i++)
+		tree.insert(elem[i]);
+*/
+		if (!in) return (1);
+
+        cout << hex << uppercase;
+		E e;
+		char str[16], *endptr;
+		while (!in.eof()) {
+			in >> str;
+			e = strtol(str, &endptr, 16);
+			// eofのみでファイルの終わりを正確に検出できないので、終了の印-1を用いる
+			if (e == -1) break;
+            cout << right << setfill('0') << setw(2);
+            cout << e << ":";
+			auto pr = tree.insert(e);
+			// 同じ数値を入力するとそれを削除する
+			if (!pr.first) {
+				tree.erase(pr.second);
+                cout << right << setfill('0') << setw(2);
+				cout << e << " erased." << std::endl;
+			}
+			else {
+                cout << right << setfill('0') << setw(2);
+				cout << e << " inserted." << endl;
+			}
+		}
+	}
+	// １６進、大文字使用、右詰め、０埋め、２桁
+	dsp.out();
+	return 0;
+}
+
---
+++ rbtree/rbtree.h
@@ -0,0 +1,244 @@
+/* http://algoogle.hadrori.jp/algorithm/rbtree.html
+ * 修正ポイント
+ * (1) nil の再定義
+ *     ・nil は特定の値を持たない
+ *     ・常に黒を指す
+ * (2) 削除直後の修正は子供がいるかいないかの大きく２通り
+ * (3) 削除後の全般修正は削除したノードの親を中心に実施
+ * (4) print関数を削除して、外部にもっと凝ったものを実装する
+ */ 
+
+//#include <iomanip> //　マニピュレータ
+using namespace std;
+
+template<class T> class rbtree {
+public:
+    struct node {
+        T key;
+        int red;
+        node *par, *ch[2]; // {left, right}
+        node() {}
+        node(T key):key(key),red(1) {
+            //par = ch[0] = ch[1] = NULL;
+        }
+        // 自分が親から見てどちら側の子か取得(0=左、1=右)
+        int getChildPos() const { return (this == par->ch[0])? 0: 1; }
+    };
+
+    unsigned sz;
+    node *root, *nil;
+
+    rbtree():sz(0) {
+        nil = new node();
+        nil->red = 0;
+        root = nil->par = nil->ch[0] = nil->ch[1] = nil;
+    }
+
+    unsigned size() const { return sz; }
+
+    // xを中心にd方向に回転
+    // コメントはわかりやすいようにd=0の場合に統一
+    void rotate(node *x, int d) {
+        int e = d^1;
+        node *y = x->ch[e];
+        node *t = y->ch[d];
+        // yの左の子をxの右の子にする
+        x->ch[e] = t;
+        if (t != nil) t->par = x;
+        // yをxの親の子にする
+        y->par = x->par;
+        if(y->par == nil) root = y;
+        else x->par->ch[x->getChildPos()] = y;
+        // xをyの左の子にする
+        y->ch[d] = x;
+        x->par = y;
+    }
+
+    // 同一キーをもつノードを返す
+    node *find(T key) {
+        node *x = root;
+        while(x != nil) {
+            if(key == x->key) return x;
+            x = (key < x->key)? x->ch[0]: x->ch[1];
+        }
+        return x;
+    }
+
+    // 木に新しい要素を追加する。すでにキーが登録されている場合追加できない
+    pair<bool, node *> insert(T key) {
+        node *x = root, *y = nil;
+        int d;
+        // キーでノードを検索
+        while(x != nil) {
+            if(key == x->key) 
+                // すでにキーが登録していたら、そのノードを返す
+                return make_pair(false, x);
+            y = x;
+            x = (key < x->key)? x->ch[0]: x->ch[1];
+        }
+        
+        // 見つからないので新規作成
+        sz++;
+        node *z = new node(key);
+        if (sz == 1) {
+            z->par = z->ch[0]= z->ch[1] = nil;
+            root = z;
+        }
+        else {
+            ((key < y->key)? y->ch[0]: y->ch[1]) = z;
+            z->par = y;
+            z->ch[0] = z->ch[1] = nil;
+        }
+        insert_update(z);
+        return make_pair(true, z);
+    }
+
+    // 追加後の木を修正する
+    void insert_update(node *z) {
+        node *y;
+        // 親yが赤の間繰り返す
+        while((y = z->par)->red) {
+            int d = y->getChildPos(), e = d^1;
+            // xはzのおじ
+            node *x = y->par->ch[e];
+            if(x->red) {
+                // ①おじが赤の場合
+                y->red = 0;
+                x->red = 0;
+                y->par->red = 1;
+                z = y->par;
+            }
+            else {
+                // ②おじが黒の場合
+                if(z == y->ch[e]) {
+                    // ②→③
+                    z = y;
+                    rotate(z,d);
+                }
+                else {
+                    // ③
+                    y->red = 0;
+                    y->par->red = 1;
+                    rotate(y->par,e);
+                }
+            }
+        }
+        root->red = 0;
+    }
+
+    // 木から要素を削除
+    void erase(T key) { erase(find(key)); }
+
+    // 指定ノードを削除する
+    void erase(node *z) {
+        if(z == nil) return;
+        sz--;
+        node *w;                // 修正するノードの親（修正する部分木の根）ノード
+        int d;                  // 修正するノードは左右どちらの子か
+        int ex_col;             // 除去するノードの色を記憶
+        if (z->ch[0] == nil || z->ch[1] == nil) {
+            // 削除ノードにこどもがいないか1人だけ
+            int e = (z->ch[0] == nil? 1: 0);    // こどもがいる(可能性がある)方はどっち？
+            w = z->par;
+            d = z->getChildPos();
+            ex_col = z->red;
+            replace(z, z->ch[e]);
+        }
+        else {
+            // 削除ノードにこどもが2人
+            node *y;            // 部分木の最小ノードの位置
+            y = minimum(z->ch[1]);
+            ex_col = y->red;
+            if(y->par == z) {
+                // yとzが直結する場合は左斜め上に平行移動
+                w = y;
+                d = 1;
+            }
+            else {
+                // yとzが離れている場合はyを木構造より除去
+                w = y->par;
+                d = 0;
+                replace(y, y->ch[1]);
+                // 削除ノードzの右の子を引き継ぐ
+                y->ch[1] = z->ch[1];
+                y->ch[1]->par = y;
+            }
+            replace(z,y);
+
+            // 削除ノードの左の子を引き継ぐ
+            y->ch[0] = z->ch[0];
+            y->ch[0]->par = y;
+            y->red = z->red;
+        }
+        
+        // 黒ノードを除去した場合は修正に入る
+        if(!ex_col) erase_update(w, d);
+        
+        delete z;
+    }
+
+    // 部分木uの親ノードに部分木vを接続
+    void replace(node *u, node *v) {
+        node *y = u->par;
+        if (y == nil) root = v;
+        else y->ch[u->getChildPos()] = v;
+        if (v != nil) v->par = y;
+    }
+    
+    // 部分木xの最小ノードを取得
+    node *minimum(node* x) {
+        while(x->ch[0] != nil) x = x->ch[0];
+        return x;
+    }
+
+    // 削除後の木を修正する
+    // dはその子x がどちらの位置にあるか
+    void erase_update(node *y, int d) {
+        node *x;
+        while(y != nil && !(x = y->ch[d])->red) {
+            int e = d^1;
+            node *w = y->ch[e];
+            if(w == nil) {
+                // 兄弟ノードは存在しない
+                d = y->getChildPos();
+                y = y->par;
+            }
+            if(w->red) {
+                // ①兄弟ノードが赤の場合
+                // ①→②～④
+                w->red = 0;
+                y->red = 1;
+                rotate(y,d);
+            }
+            else {
+                // ②～④兄弟ノードが黒の場合
+                if(!w->ch[0]->red and !w->ch[1]->red) {
+                    // ②
+                    w->red = 1;
+                    d = y->getChildPos();
+                    y = y->par;
+                }
+                else if(!w->ch[e]->red) {
+                    // ③兄弟中心に逆回転
+                    // ③→④ ２段階回転
+                    w->red = 1;
+                    w->ch[d]->red = 0;
+                    rotate(w,e);
+                }
+                else {
+                    // ④部分木の根中心に順回転
+                    // ④→修正終了
+                    w->red = y->red;
+                    y->red = 0;
+                    w->ch[e]->red = 0;
+                    rotate(y,d);
+                    x = root;
+                    break;
+                }
+            }
+
+        }
+        x->red = 0;
+    }
+};
+
